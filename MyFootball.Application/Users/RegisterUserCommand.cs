62204662884104bf3a9976de9665693694c16c7c
Steeven <steevensundanum@gmail.com>
2025-07-04 23:30:22 +0400

    Push the application layer

---
+++ MyFootball.Application/Users/RegisterUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace MyFootball.Application.Users
+{
+    public class RegisterUserCommand : IRequest<string>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
